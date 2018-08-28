8b97c942f8ae7f19d496a7224bb3b1283aca7703
Vivien Renee <vivien.renee.b@gmail.com>
2018-08-27 22:12:10 -0700

    Add files via upload

---
+++ PG94Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+namespace PG94
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Pick a number");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            bool RightNum = Num1 == 7;
+
+            while (!RightNum)
+            {
+                switch (Num1)
+                {
+                    case 1:
+                        Console.WriteLine("Wrong!");
+                        Console.WriteLine("Pick a number");
+                        Num1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("Wrong!");
+                        Console.WriteLine("Pick a number");
+                        Num1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("Right!");
+                        RightNum = true;
+                        break;
+                    default:
+                        Console.WriteLine("You're bad at this!");
+                        Console.WriteLine("Pick a number");
+                        Num1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+
+
+            Console.WriteLine("Time For Round 2!");
+            Console.WriteLine("Pick a number!");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+            bool RightNum2 = Num2 == 6;
+            do
+            {
+                switch (Num2)
+                {
+                    case 1:
+                        Console.WriteLine("Wrong!");
+                        Console.WriteLine("Pick a number");
+                        Num2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("Wrong!");
+                        Console.WriteLine("Pick a number");
+                        Num2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        Console.WriteLine("Right! Game over!");
+                        RightNum2 = true;
+                        break;
+                    default:
+                        Console.WriteLine("You're bad at this!");
+                        Console.WriteLine("Pick a number");
+                        Num2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+
+            while (!RightNum2);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
