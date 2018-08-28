91be61fcdf196e84d94e356bd18aeffd9585a490
Vivien Renee <vivien.renee.b@gmail.com>
2018-08-27 19:50:43 -0700

    Add files via upload

---
+++ PG75Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PG75Program.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age");
+            string PersonAge = Console.ReadLine();
+            int Age = Convert.ToInt32(PersonAge);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string DUIAnswer = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(DUIAnswer);
+           
+            Console.WriteLine("How many speeding tickets do you have?");
+            string TicketNumber = Console.ReadLine();
+            int Ticket = Convert.ToInt32(TicketNumber);
+
+            bool Qualified = Age > 15 && DUI == false && Ticket < 4;
+
+            Console.WriteLine("Qualified? " + Qualified);
+
+
+            Console.ReadLine();
+        }
+    }
+}
