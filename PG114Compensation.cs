2adc57d9a48b3813dd75b994bf0cfa419af36cfb
Vivien Renee <vivien.renee.b@gmail.com>
2018-09-22 19:12:54 -0700

    Add files via upload

---
+++ PG114Compensation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG114Program.cs
+{
+   public class Compensation
+    {
+        //public int payCheck(int hoursWorked)
+        //{
+        //    int weeklyPay = hoursWorked * 6;
+        //    return weeklyPay;
+        //}
+
+        //public int payCheck(decimal hoursWorked)
+        //{
+        //    int weeklyPay = Decimal.ToInt32(hoursWorked) * 6;
+        //    return weeklyPay;
+        //}
+
+        public int payCheck(string hoursWorked)
+        {
+            int weeklyPay =Convert.ToInt32(hoursWorked) * 6;
+            return weeklyPay;
+        }
+
+
+
+    }
+}
