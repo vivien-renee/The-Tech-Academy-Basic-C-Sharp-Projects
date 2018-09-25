c7fe7d3e1a2ea64feeee3ea7cd31c58ca2802ad6
Vivien Renee <vivien.renee.b@gmail.com>
2018-09-25 15:29:34 -0700

    Add files via upload

---
+++ PG113Compensation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG113Program.cs
+{
+    class  Compensation
+    {
+
+        
+        public void PayCheckCalculation (int hoursWorked,  int payRate)
+        {
+
+            int baseRatePay = hoursWorked * payRate;
+
+            
+            Console.WriteLine("Your pay per hour is: $" + payRate);
+
+
+        }
+    }
+}
---
+++ PG113Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG113Program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Compensation compensation = new Compensation();
+            
+           
+            
+            compensation.PayCheckCalculation(hoursWorked: 2, payRate: 3); 
+            
+            
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
