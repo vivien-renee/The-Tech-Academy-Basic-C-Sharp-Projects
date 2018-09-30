2b4c959a71873afa1c5ae42c5ce1a9d04b0f7a80
Vivien Renee <vivien.renee.b@gmail.com>
2018-09-30 12:58:09 -0700

    Add files via upload

---
+++ PG117Favorites.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG117Program.cs
+{
+    public static class Favorite
+
+    {
+        public static int howMuch;
+        
+        static Favorite()
+
+        {
+
+            howMuch = 100;
+
+
+    }
+    }
+}
---
+++ PG117Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG117Program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Pie pie = new Pie();
+            //int myHalf;
+            int myDesserts;
+            
+
+
+
+
+            Console.WriteLine("How many pies do we have?");
+            int numberOfPies = Convert.ToInt32(Console.ReadLine());
+
+            pie.MyShare(numberOfPies);
+
+
+
+
+            myDesserts = pie.Desserts(numberOfPies);
+
+            Console.WriteLine("I will eat " + myDesserts + " pies today");
+
+            int value = Favorite.howMuch;
+
+            Console.WriteLine("I love dessert " + value + "% of the time!");
+            Console.ReadLine();
+
+        }
+    }
+}
