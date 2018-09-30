ab936bc4f8272f73a40f0d5e5f35f669148abfb6
Vivien Renee <vivien.renee.b@gmail.com>
2018-09-30 16:03:27 -0700

    Add files via upload

---
+++ PG120Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG120Program.cs
+{
+   public class Person
+    {
+        
+        public List<string> FirstName{ get; set; } //These are the properties
+        public List<string> LastName { get; set; }
+        
+
+        public void SayName() //This is the method name
+        {
+           
+
+            foreach (string FName in FirstName)
+                foreach (string LName in LastName)
+                {
+                Console.WriteLine("Name: "  + FName + " " + LName);
+                
+            }
+        }
+            
+    }
+}
---
+++ PG120Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG120Program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+
+
+            person.FirstName = new List<string>() { "Sample"  };
+            person.LastName = new List<string>() { "Student" };
+            person.SayName();
+
+            Console.ReadLine();
+        }
+    }
+
+}
