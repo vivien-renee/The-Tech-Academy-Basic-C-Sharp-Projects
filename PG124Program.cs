7963856a8aab00b4f0a52e3021e2e8b5e1c3af27
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-02 15:01:32 -0700

    Add files via upload

---
+++ PG124Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG124Program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee ();
+            person.firstName = "Sample";
+            person.lastName = "Student";
+            person.SayName();
+
+            Console.ReadLine();
+        }
+
+        public abstract class Person
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public abstract void SayName();
+        }
+
+        public class Employee : Person
+        {
+            public override void SayName()
+            {
+                Console.WriteLine("Name: " + firstName + " " + lastName);
+            }
+
+        }
+    }
+}
