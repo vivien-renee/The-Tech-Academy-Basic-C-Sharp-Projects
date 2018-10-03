5720212efbd964aead33d42acc1f13d51d5b7d6e
Vivien Renee <vivien.renee.b@gmail.com>
2018-10-02 18:01:44 -0700

    Add files via upload

---
+++ PG126Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG126Program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.firstName = "Sample";
+            person.lastName = "Student";
+            person.SayName();
+
+            IQuittable quittable = new Employee();
+            quittable.Quit();
+
+            Console.ReadLine();
+        }
+
+        public abstract class Person
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public abstract void SayName();
+        }
+
+        public class Employee : Person, IQuittable
+        {
+            public override void SayName()
+            {
+                Console.WriteLine("Name: " + firstName + " " + lastName);
+            }
+            public void Quit()
+            {
+                Console.WriteLine("I Quit.");
+            }
+        }
+
+        interface IQuittable
+        {
+            void Quit();
+        }
+
+
+
+
+    }
+    
+}
